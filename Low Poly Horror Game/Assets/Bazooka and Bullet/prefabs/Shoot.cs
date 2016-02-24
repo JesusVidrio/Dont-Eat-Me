0e83b98461e4a50d84af4eb9652a130f6fa14a94
JesusVidrio <JesusVidrio18@gmail.com>
2016-02-23 18:01:10 -0800

    Added files via upload

---
+++ Low Poly Horror Game/Assets/Bazooka and Bullet/prefabs/Shoot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bullet;
+	public GameObject bulletHole;
+    public float speed;
+	public float delayTime = 0.5f;
+	
+	private float counter = 0;
+	
+	void FixedUpdate ()	
+ 	{
+		if(Input.GetKey(KeyCode.Mouse0) && counter > delayTime)
+		{
+			Instantiate(bullet, transform.position, transform.rotation, transform.forward * speed);
+			counter = 0;
+			
+			RaycastHit hit;
+			Ray ray = new Ray(transform.position, transform.forward);
+			if(Physics.Raycast(ray, out hit, 100f))
+			{
+				Instantiate(bulletHole, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+			}
+		}
+		counter += Time.deltaTime;
+	}
+
+    private void Instantiate(GameObject bullet, Vector3 position, Quaternion rotation, Vector3 vector3)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Low Poly Horror Game/Assets/BulletSpeedScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletSpeedScript : MonoBehaviour {
+
+    public Vector3 movementVector;  //unit vector 
+
+    public float speed;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Destroy(gameObject, 3);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + movementVector * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        Debug.Log("Hit something!");
+        if (collider.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Hit an enemy!");
+            collider.gameObject.GetComponent<EnemyStats>().takeDamage(2f);
+        }
+    }
+
+}
